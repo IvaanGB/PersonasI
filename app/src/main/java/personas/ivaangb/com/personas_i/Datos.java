a8a9de050da7a32bfe87830e0602436f29f29888
IvaanGB <igonzale4@cuc.edu.co>
2018-04-09 21:05:39 -0500

    Aplicacion terminada sin validaciones ni traducción

---
+++ app/src/main/java/personas/ivaangb/com/personas_i/Datos.java
@@ -0,0 +1,20 @@
+package personas.ivaangb.com.personas_i;
+
+import java.util.ArrayList;
+
+
+public class Datos {
+
+    private static ArrayList<Persona> personas = new ArrayList<>();
+
+    public static void guardar (Persona p){
+        personas.add(p);
+    }
+
+    public static ArrayList<Persona> obtener(){
+        return personas;
+    }
+
+
+
+}
--- app/src/main/java/personas/ivaangb/com/personas_i/Registrar.java
+++ app/src/main/java/personas/ivaangb/com/personas_i/Registrar.java
@@ -1,13 +1,46 @@
 package personas.ivaangb.com.personas_i;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Registrar extends AppCompatActivity {
+    private EditText txtCedula, txtNombre, txtApellido;
+    private Resources recursos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+
+        recursos = getResources();
+        txtCedula = findViewById(R.id.txtCedula);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtApellido = findViewById(R.id.txtApellido);
+    }
+
+    public void guardar(View v){
+        String ced, nomb, apell;
+        ced = txtCedula.getText().toString();
+        nomb = txtNombre.getText().toString();
+        apell = txtApellido.getText().toString();
+
+        Persona p = new Persona(ced, nomb, apell);
+        p.guardar();
+        Toast.makeText(this, recursos.getString(R.string.mensaje_guardado), Toast.LENGTH_SHORT).show();
+        borrar();
+    }
+
+    public void limpiar(View v){
+        borrar();
+    }
+
+    public void borrar(){
+        txtCedula.setText("");
+        txtNombre.setText("");
+        txtApellido.setText("");
     }
 }
